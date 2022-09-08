34800fbc6c45a4be47474ab6f96ca7db80f57a9d
Michelle <Michelle@Johns-MacBook-Pro-2.local>
2022-09-08 14:04:20 -0400

    Task A

--- valid.cpp
+++ valid.cpp
@@ -13,11 +13,13 @@ int main()
 	int x;
 	std::cout << "Please enter an integer: ";
 	std::cin >> x;
+
 	while (x<=0 || x>=100)
 	{ 
 	std::cout << "Please re-enter: ";
 	std::cin >> x;
 	}
+
 	int sq = x*x;
 	std::cout << "Number squared is "+ std::to_string(sq) << std::endl;
 	return 0;
