c6ebf7b903dda019f6cccb34178c5a45ee9180c3
Michelle <Michelle@Johns-MacBook-Pro-2.local>
2022-09-08 11:14:38 -0400

    Task C

---
+++ edit-array.cpp
@@ -0,0 +1,43 @@
+/*
+Author: Michelle Wen
+Course: CSCI-135
+Instructor: Mike Zamansky
+Assignment: Lab 2 Task C
+
+Create an arry and allow user to edit.
+*/
+
+#include <iostream>
+int main()
+{
+	int myData[10];
+	for(int i=0; i<10; i++)
+	{
+		myData[i]=1;
+	}
+	for(int i=0; i<sizeof(myData)/sizeof(int); i++)
+	{
+		std::cout << std::to_string(myData[i])+" ";
+	}
+	std::cout<< "" << std::endl;
+	
+	int ind=0;
+	do {
+		int val;
+		std::cout << "Input index: ";
+		std::cin >> ind;
+		std::cout << "Input value: ";
+		std::cin >> val;
+		myData[ind]=val;
+		for(int i=0; i<sizeof(myData)/sizeof(int); i++)
+        	{
+                	std::cout << std::to_string(myData[i])+" ";
+        	}
+        	std::cout<< "" << std::endl;
+			
+	} while (ind>=0 && ind<(sizeof(myData)/sizeof(int)));	
+
+	std::cout << "Index out of range. Exit." << std::endl;
+
+	return 0;
+}
